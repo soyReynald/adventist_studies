5bab17d62ea305545d6cbc48d092899936d0c19e
Reynald Rodriguez <soyreynald@outlook.com>
2019-07-24 17:52:04 -0400

    Initial commit - App.py - by: Reynald

---
+++ app.py
@@ -0,0 +1,11 @@
+# Developed by Reynald R.
+# Work reporter - -> 07/24/2019 - 9/22/2019 (Python -TO- Django)
+print("This is an app to generate reports and statistics")
+options = int(input("Seleccione el tipo de reporte que desea generar"))
+
+if options  == 1:
+    print("You've chosen a good option, we may continue")
+
+
+
+input("Press enter to close") # To close
